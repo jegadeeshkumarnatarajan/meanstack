9e226823fa0b5626a831165ccf481bc21a14fe43
Jegadeesh <ayrjegan@gmail.com>
2016-09-27 05:51:31 +0530

    Added factory, router, templates

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # meanstack
+
+ based on tutorial - https://thinkster.io/mean-stack-tutorial#angular-routing
+
+enabling to Node project:
+------------------------------
+npm install -g express-generator
+express --ejs flapper-news
+cd flapper-news
+npm install
+npm install --save mongoose
+
+
+First move the index.html file to the views/ directory.
+Because we're using the ejs engine, Node is looking for files with the .ejs extension
+so we're going to have to rename our index.html to index.ejs, replacing the existing one.
+
+Next, move the Angular app.js file to the public/javascripts/ directory.
+To avoid confusion with Node's app.js, also rename the Angular file to angularApp.js.
+
+Finally let's update the <script> tag in our index.ejs file to reflect these changes:
+
+<script src="/javascripts/angularApp.js"></script>
+
+Now we can start our application by running npm start.
+
+If we point our browser to http://localhost:3000
--- app.js
+++ app.js
@@ -1,28 +1,78 @@
-var app=angular.module("flapperNews",[]);
+var app=angular.module("flapperNews",['ui.router']);
+
+app.config([
+  '$stateProvider',
+  '$urlRouterProvider',
+  function($stateProvider,$urlRouterProvider){
+    $stateProvider
+    .state('home',{
+      url: "/home",
+      templateUrl: "/home.html",
+      controller:"MainCtrl"
+    })
+    .state('posts', {
+      url: '/posts/{id}',
+      templateUrl: '/posts.html',
+      controller: 'PostsCtrl'
+    });
+    $urlRouterProvider.otherwise('home');
+  }
+]);
+
+app.factory("posts", function(){
+  //service body
+  var o ={
+    posts: []
+  };
+  return o;
+});
 
 app.controller("MainCtrl",[
   '$scope',
-  function($scope){
+  'posts',
+  function($scope,posts){
     $scope.test="Hello World!";
-    $scope.posts = [
-      {title:'post 1', upvotes:5},
-      {title:'post 2', upvotes:3},
-      {title:'post 3', upvotes:1},
-      {title:'post 4', upvotes:4},
-      {title:'post 5', upvotes:1}
-];
+    $scope.posts = posts.posts;
 
-$scope.addPost = function(){
-  //$scope.posts.push({title:"A new Post",upvotes:0});
-  if(!$scope.title || $scope.title === '') { return; }
-  $scope.posts.push({title:$scope.title,link:$scope.link,upvotes:0});
-  $scope.title='';
-  $scope.link='';
-};
+    $scope.addPost = function(){
+            //$scope.posts.push({title:"A new Post",upvotes:0});
+            if(!$scope.title || $scope.title === '') { return; }
+            //$scope.posts.push({title:$scope.title,link:$scope.link,upvotes:0});
 
-$scope.incrementVotes = function(post){
-  post.upvotes+=1;
-};
+                  $scope.posts.push({
+                    title: $scope.title,
+                    link: $scope.link,
+                    upvotes: 0,
+                    comments: [
+                      {author: 'Joe', body: 'Cool post!', upvotes: 0},
+                      {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+                    ]
+                  });
 
+            $scope.title='';
+            $scope.link='';
+    };
+
+    $scope.incrementVotes = function(post){
+      post.upvotes+=1;
+    };
   }
 ]);
+
+
+app.controller('PostsCtrl', [
+'$scope',
+'$stateParams',
+'posts',
+function($scope, $stateParams, posts){
+      $scope.post = posts.posts[$stateParams.id];
+      $scope.addComment = function(){
+      if($scope.body === '') { return; }
+      $scope.post.comments.push({
+        body: $scope.body,
+        author: 'user',
+        upvotes: 0
+      });
+      $scope.body = '';
+    };
+}]);
